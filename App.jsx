21b9955c9041762baf685858b2546818fbd92853
DeveloperYamin <developer.yamin@gmail.com>
2022-03-05 22:34:16 +0600

    add SafeAreaProvider

--- App.jsx
+++ App.jsx
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
-import HomeScreen from './screens/HomeScreen';
-import { store } from './store';
+import HomeScreen from "./screens/HomeScreen";
+import { store } from "./store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen/>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
-};
+}
