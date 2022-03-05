de8ab74d206f7b3df1995f8d96adce21f1488a80
DeveloperYamin <developer.yamin@gmail.com>
2022-03-05 21:50:21 +0600

    add demo data

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -1,5 +1,19 @@
 import { Text, View } from "react-native";
 
+const data = [
+     {
+          id: "123",
+          title: "Get a ride",
+          image: "https://Links.papareact.con/3pn",
+          screen: "MapScreen",
+     }, {
+          id: "456",
+          title: "Order food",
+          image: "https://links.papareact.con/28w",
+          screen: "EatsScreen", // Change in future...
+     },
+];
+
 const NavOptions = () => {
   return (
     <View>
