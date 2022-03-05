64637534dfc00b2c362e10798b6f8b74d9a355aa
DeveloperYamin <developer.yamin@gmail.com>
2022-03-05 21:41:11 +0600

    create components folder add NavOptions.jsx file

---
+++ components/NavOptions.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const NavOptions = () => {
+  return (
+    <View>
+      <Text>NavOptions</Text>
+    </View>
+  );
+};
+
+export default NavOptions;
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -4,7 +4,7 @@ import tw from "tailwind-react-native-classnames";
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <View>
+      <View style={tw`p-5`}>
         <Image
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{ uri: "https://links.papareact.com/gzs" }}
