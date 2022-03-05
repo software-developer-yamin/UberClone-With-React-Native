85d3ce0ed0972537ab67c60776d03da508c2e6dd
DeveloperYamin <developer.yamin@gmail.com>
2022-03-05 22:47:42 +0600

    Building the NavOptions Component

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -1,5 +1,6 @@
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
-import tw from "tailwind-react-native-classnames"
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
 
 const data = [
   {
@@ -24,15 +25,19 @@ const NavOptions = () => {
         keyExtractor={(item) => item.id}
         horizontal
         renderItem={({ item }) => (
-             <TouchableOpacity
-                  style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
-             >
+          <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
             <View>
               <Image
                 style={{ width: 120, height: 120, resizeMode: "contain" }}
                 source={{ uri: item.image }}
               />
-              <Text style={tw`m-2 font-semibold text-lg`}  >{item.title}</Text>
+              <Text style={tw`m-2 font-semibold text-lg`}>{item.title}</Text>
+              <Icon
+                style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                name="arrowright"
+                color="white"
+                type="antdesign"
+              />
             </View>
           </TouchableOpacity>
         )}
