a5e083c59fb5cc8dd174ce406958482284e4a050
DeveloperYamin <developer.yamin@gmail.com>
2022-03-06 17:13:56 +0600

    set environmentenvironment variable

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    Plugin: [
+      "module:react-native-dotenv",
+      {
+        "moduleName": "@env",
+        "path": ".env",
+      },
+    ]
   };
 };
--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,12 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View } from "react-native";
 
 const MapScreen = () => {
   return (
     <View>
       <Text>MapScreen</Text>
     </View>
-  )
-}
+  );
+};
 
 export default MapScreen;
