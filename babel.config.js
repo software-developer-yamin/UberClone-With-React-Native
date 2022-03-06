02b0b97961c2731bb4e710923a620e5a1a340cff
DeveloperYamin <developer.yamin@gmail.com>
2022-03-06 22:06:08 +0600

    Implementing Google Autocomplete Library

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    Plugin: [
-      "module:react-native-dotenv",
-      {
-        "moduleName": "@env",
-        "path": ".env",
-      },
-    ]
   };
 };
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,14 +1,55 @@
 import { Image, SafeAreaView, View } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { useDispatch } from "react-redux"
+import {setDestination, setOrigin} from "../slice/navSlice"
+
+const demoData = {
+  location: {
+    lat: 51.509865,
+    lng: -0.118092,
+  },
+  description:
+    "London is a political, historical, cultural, and tourist center of the United Kingdom",
+};
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where From Search?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(setOrigin({
+              location: details.geometry.location || demoData.location,
+              description: data.description || demoData.description,
+            }));
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          minLength={2}
+          enablePoweredByContainer={false}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          query={{
+            key: "AIzaSyDuLx1ibdxs3MZCvyoFqjfh6J0kRzc_c6E",
+            language: "en",
+          }}
         />
         <NavOptions />
       </View>
