82432e40725d5783f0da81d3007f80c31c6cecad
DeveloperYamin <developer.yamin@gmail.com>
2022-03-06 15:58:26 +0600

    Implementing React Native Navigation

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { Icon } from "react-native-elements";
 import tw from "tailwind-react-native-classnames";
@@ -18,6 +19,7 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
   return (
     <View>
       <FlatList
@@ -25,7 +27,10 @@ const NavOptions = () => {
         keyExtractor={(item) => item.id}
         horizontal
         renderItem={({ item }) => (
-          <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate(item.screen)}
+            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          >
             <View>
               <Image
                 style={{ width: 120, height: 120, resizeMode: "contain" }}
--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,5 +1,12 @@
-function MapScreen() {
-  return <div>MapScreen</div>;
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>MapScreen</Text>
+    </View>
+  )
 }
 
 export default MapScreen;
