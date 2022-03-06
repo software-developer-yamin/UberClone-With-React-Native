aa4e47b06b63345272bbb0b471cabd3f6d4c48c7
DeveloperYamin <developer.yamin@gmail.com>
2022-03-06 14:39:38 +0600

    create screens

--- App.jsx
+++ App.jsx
@@ -1,15 +1,29 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
 import { store } from "./store";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
